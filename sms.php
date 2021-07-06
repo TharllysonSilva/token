335c1ad00f49619e265d87ced0b7b77cb6275556
TharllysonSilva <44323993+TharllysonSilva@users.noreply.github.com>
2021-07-06 09:47:31 -0300

    Criação para enviar o token pelo COMTELE

---
+++ sms.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once "vendor/autoload.php";
+use Comtele\Services\TokenService;
+const API_KEY = "";
+
+$numero = isset($params['numero']) != '' ? $params['numero'] : '';
+$token  = isset($params['token'])  != '' ? $params['token']  : '';
+
+$tokenService = new TokenService(API_KEY);
+
+if($action == "sentToken"){
+	$result = $tokenService->send_totken(
+		$numero,  // PhoneNumber: Numero de telefone que vai ser enviado o token via SMS.
+		"Unifacema"     // Prefix: Identificação da empresa que aparecerá no corpo da mensagem. 
+	);
+	echo json_encode($result);
+}
+
+if($action == "validToken"){
+	$result = $tokenService->validate_token(
+	  $token   // TokenCode: Token que o cliente informou e deve ser validado pela Comtele.
+	);
+	echo json_encode($result);
+	
+}
+
+
+	
+?>
